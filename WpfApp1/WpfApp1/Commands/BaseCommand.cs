507ffc74f19936b4c1f29e5cc9927132c0206c2c
Jakub <kuba24481@wp.pl>
2024-03-27 15:52:20 +0100

    Create DataBase and add create connection between the database and app

--- WpfApp1/WpfApp1/Commands/BaseCommand.cs
+++ WpfApp1/WpfApp1/Commands/BaseCommand.cs
@@ -3,10 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace WpfApp1.Commands
 {
-   public class BaseCommand
+   public abstract class BaseCommand:ICommand
     {
+        public event EventHandler CanExecuteChanged;
+
+        public virtual bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public abstract void Execute(object parameter);
+
+
+        public void OnCanExecuteChanged()
+        {
+            CanExecuteChanged?.Invoke(this, new EventArgs());
+        }
     }
 }
